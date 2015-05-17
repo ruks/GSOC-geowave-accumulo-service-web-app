29f57cd5f16249b1ebbbbdc857a9ef5cf418a9dd
rukshan <ruky68@live.com>
2015-05-17 19:06:22 +0530

    added d3 simple app based on accumulo data

---
+++ src/main/java/com/eclipse/geowave/accumulo/app/d3test.java
@@ -0,0 +1,31 @@
+package com.eclipse.geowave.accumulo.app;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class d3test
+ */
+public class d3test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public d3test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("index.html").forward(request, response);
+	}
+
+}
